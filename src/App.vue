7e7140ed5c872d208f1bd87833ee8a281f7a2851
TJ Phan <t.jamesphan@gmail.com>
2024-01-31 10:33:53 -0800

    Add layout header and route links

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
-  <router-view></router-view>
+	<the-header></the-header>
+	<router-view></router-view>
 </template>
 
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+export default {
+	components: {
+		TheHeader,
+	},
+};
+</script>
+
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 * {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+	font-family: 'Roboto', sans-serif;
 }
 
 body {
-  margin: 0;
+	margin: 0;
 }
 </style>
