65a3a85c4acf0680df3a3c786454d2cb471d2121
Glitjch <73555664+glitjch@users.noreply.github.com>
2024-02-12 11:21:39 -0800

    Start on MentorRegistration component. Layout of form and preparing for connecting to Storage

--- src/pages/mentors/MentorRegistration.vue
+++ src/pages/mentors/MentorRegistration.vue
@@ -1,3 +1,78 @@
 <template>
-  <section>Register</section>
+	<base-card>
+		<section>
+			<h2>Register as a Mentor</h2>
+		</section>
+		<section>
+			<span>
+				<label for="firstName">First Name</label>
+				<input id="firstName" />
+			</span>
+			<span>
+				<label for="lastName">Last Name</label>
+				<input id="lastName" />
+			</span>
+			<div>
+				<h3>Areas of Expertise</h3>
+				<p>Select all that apply</p>
+				<p>{{ this.areas }}</p>
+				<span class="filter-option">
+					<input
+						type="checkbox"
+						id="frontend"
+						value="frontend"
+						v-model="areas"
+					/>
+					<label for="frontend">Front end</label>
+				</span>
+				<span class="filter-option">
+					<input type="checkbox" id="backend" value="backend" v-model="areas" />
+					<label for="backend">Back end</label>
+				</span>
+				<span class="filter-option">
+					<input type="checkbox" id="career" value="career" v-model="areas" />
+					<label for="career">Career</label>
+				</span>
+			</div>
+		</section>
+	</base-card>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+      areas: [],
+      description: "",
+      hourlyRate: 0,
+		};
+	},
+};
+
+// Add actions
+// add submit button
+// make sure to redirect to home page (list of mentors)
+</script>
+
+<style scoped>
+h2 {
+	margin: 0.5rem 0;
+}
+
+.filter-option {
+	margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+	vertical-align: middle;
+}
+
+.filter-option label {
+	margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+	font-weight: bold;
+}
+</style>
