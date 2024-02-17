a847ee9a66f059dfdf18fa9f5e12c9123086f111
Glitjch <73555664+glitjch@users.noreply.github.com>
2024-02-16 18:12:45 -0800

    Update page for ContactMentor with a form, including script and styling for submission

--- src/pages/requests/ContactMentor.vue
+++ src/pages/requests/ContactMentor.vue
@@ -1,3 +1,116 @@
 <template>
-  <section>Contact Mentorr</section>
+	<form @submit.prevent="submitMessage">
+		<div class="form-control" :class="{ errors: !email.isValid }">
+			<label for="email">Your Email</label>
+			<input
+				type="email"
+				id="email"
+				v-model.trim="email.val"
+				@blur="clearValidity('email')"
+			/>
+			<p v-if="!email.isValid">Please provide a proper email.</p>
+		</div>
+		<div class="form-control" :class="{ errors: !message.isValid }">
+			<label for="message">Your Message</label>
+			<textarea
+				rows="5"
+				id="message"
+				v-model.trim="message.val"
+				@blur="clearValidity('message')"
+			></textarea>
+			<p v-if="!message.isValid">Message cannot be empty.</p>
+		</div>
+		<div class="actions">
+			<base-button>Send Message</base-button>
+		</div>
+	</form>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			email: {
+				val: '',
+				isValid: true,
+			},
+			message: {
+				val: '',
+				isValid: true,
+			},
+			formIsValid: true,
+		};
+	},
+	methods: {
+		clearValidity(input) {
+			this[input].isValid = true;
+		},
+		validateMessage() {
+			this.formIsValid = true;
+			if (this.email.val === '' || !this.email.val.includes('@')) {
+				this.email.isValid = false;
+				this.formIsValid = false;
+			}
+			if (this.message.val === '') {
+				this.message.isValid = false;
+				this.formIsValid = false;
+			}
+		},
+		async submitMessage() {
+			await this.validateMessage();
+			if (!this.formIsValid) return;
+
+			const messageData = {
+				email: this.email,
+				message: this.message,
+				date: Date.now(),
+			};
+			this.$emit('message-data', messageData);
+			return;
+		},
+	},
+};
+</script>
+<style scoped>
+form {
+	margin: 1rem;
+	border: 1px solid #ccc;
+	border-radius: 12px;
+	padding: 1rem;
+}
+
+.form-control {
+	margin: 0.5rem 0;
+}
+
+label {
+	font-weight: bold;
+	margin-bottom: 0.5rem;
+	display: block;
+}
+
+input,
+textarea {
+	display: block;
+	width: 100%;
+	font: inherit;
+	border: 1px solid #ccc;
+	padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+	border-color: #3d008d;
+	background-color: #faf6ff;
+	outline: none;
+}
+
+.errors {
+	font-weight: bold;
+	color: red;
+}
+
+.actions {
+	text-align: center;
+}
+</style>
