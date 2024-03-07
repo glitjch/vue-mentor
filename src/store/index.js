667e8f57c0b4452b60cd6099150dc5022b4303d6
Glitjch <73555664+glitjch@users.noreply.github.com>
2024-03-06 19:36:03 -0800

    Add auth module to work with vuex in preparation for http request

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,13 @@
 import { createStore } from 'vuex';
 import MentorsModule from './modules/mentors/index.js';
 import RequestsModule from './modules/requests/index.js';
+import AuthModule from './modules/auth/index.js';
 
 const store = createStore({
 	modules: {
     mentors: MentorsModule,
     requests: RequestsModule,
-	},
-	state() {
-		return {
-			userId: 'm3',
-		};
-	},
-	getters: {
-		userId(state) {
-			return state.userId;
-		},
+    auth: AuthModule,
 	},
 });
 
