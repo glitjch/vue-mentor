1658477a23186e508b84ca15fa604bc44cd90cc4
Glitjch <73555664+glitjch@users.noreply.github.com>
2024-03-08 11:40:29 -0800

    Add logic for login action for userAuth

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,12 +1,34 @@
 export default {
-	login() {
+	async login(context, payload) {
+		const { email, password } = payload;
+		const key = process.env.VUE_APP_FIREBASE_API;
+		const response = await fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key}`,
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					email,
+					password,
+					returnSecureToken: true,
+				}),
+			}
+    );
+    const responseData = await response.json();
+
+		if (!response.ok) {
+			const error = new Error(
+				responseData.error.message || 'Failed to log in'
+			);
+			throw error;
+    }
+    
 		return;
 	},
 	async signup(context, payload) {
 		const { email, password } = payload;
-    const key = process.env.VUE_APP_FIREBASE_API;
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`,
+		const key = process.env.VUE_APP_FIREBASE_API;
+		const response = await fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`,
 			{
 				method: 'POST',
 				body: JSON.stringify({
@@ -17,21 +39,22 @@ export default {
 			}
 		);
 
-    const responseData = await response.json()
+		const responseData = await response.json();
 
-    if (!response.ok) {
-			const error = new Error(responseData.error.message || 'Failed to sign up');
+		if (!response.ok) {
+			const error = new Error(
+				responseData.error.message || 'Failed to sign up'
+			);
 			throw error;
-    }
+		}
 
- 
-    const newUser = {
-      email: responseData.email,
-      userId: responseData.idToken,
-      tokenExpiration: responseData.expiresIn,
-    };
+		const newUser = {
+			email: responseData.email,
+			userId: responseData.idToken,
+			tokenExpiration: responseData.expiresIn,
+		};
 
-    context.commit('setUser', newUser)
+		context.commit('setUser', newUser);
 		return;
-	},
+  },
 };
