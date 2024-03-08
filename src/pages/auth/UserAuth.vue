38d2ab43f8da9e2e0904221b72d2ffdcf198fca4
Glitjch <73555664+glitjch@users.noreply.github.com>
2024-03-07 19:21:13 -0800

    Wrap template of user Auth with base dialog

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,24 +1,27 @@
 <template>
-	<base-card>
-		<form @submit.prevent="submitForm">
-			<div class="form-control">
-				<label for="email">E-mail</label>
-				<input type="email" id="email" v-model.trim="email" />
-			</div>
-			<div class="form-control">
-				<label for="password">Password</label>
-				<input type="password" id="password" v-model.trim="password" />
-			</div>
-			<p v-if="!formIsValid">
-				Please enter a valid email and password that must be at least 6
-				characters long.
-			</p>
-			<base-button>{{ submitButtonCaption }}</base-button>
-			<base-button type="button" mode="flat" @click="switchAuthMode">{{
-				switchModeButtonCaption
-			}}</base-button>
-		</form>
-	</base-card>
+	<div>
+    <base-dialog fixed></base-dialog>
+		<base-card>
+			<form @submit.prevent="submitForm">
+				<div class="form-control">
+					<label for="email">E-mail</label>
+					<input type="email" id="email" v-model.trim="email" />
+				</div>
+				<div class="form-control">
+					<label for="password">Password</label>
+					<input type="password" id="password" v-model.trim="password" />
+				</div>
+				<p v-if="!formIsValid">
+					Please enter a valid email and password that must be at least 6
+					characters long.
+				</p>
+				<base-button>{{ submitButtonCaption }}</base-button>
+				<base-button type="button" mode="flat" @click="switchAuthMode">{{
+					switchModeButtonCaption
+				}}</base-button>
+			</form>
+		</base-card>
+	</div>
 </template>
 
 <script>
