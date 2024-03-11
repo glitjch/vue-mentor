4982510d17ef3e93b1ddb951ba510b5d2657f5e4
Glitjch <73555664+glitjch@users.noreply.github.com>
2024-03-11 13:44:36 -0700

    Add new action to prepare for remote auth storage rather than local authentication

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -23,8 +23,8 @@ export default {
 		const returningUser = {
 			email: responseData.email,
 			userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-      token: responseData.idToken,
+			tokenExpiration: responseData.expiresIn,
+			token: responseData.idToken,
 		};
 
 		context.commit('setUser', returningUser);
@@ -64,7 +64,41 @@ export default {
 		context.commit('setUser', newUser);
 		return;
 	},
-  async logOut(context) {
+	async auth(context, payload) {
+		const mode = payload.mode;
+    let url =
+      mode === 'signup'
+        ? `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
+        : `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key}`;
+		const { email, password } = payload;
+		const key = process.env.VUE_APP_FIREBASE_API;
+		const response = await fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({
+				email,
+				password,
+				returnSecureToken: true,
+			}),
+		});
+		const responseData = await response.json();
+
+		if (!response.ok) {
+			const error = new Error(responseData.error.message || 'Failed to log in');
+			throw error;
+		}
+
+		const returningUser = {
+			email: responseData.email,
+			userId: responseData.localId,
+			tokenExpiration: responseData.expiresIn,
+			token: responseData.idToken,
+		};
+
+		context.commit('setUser', returningUser);
+
+		return;
+	},
+	async logOut(context) {
 		const payload = {
 			token: null,
 			userId: null,
